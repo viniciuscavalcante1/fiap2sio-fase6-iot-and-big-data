7ac912cdbf29f20597536982b1b52d6dedaa4186
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-10-14 15:17:51 -0300

    add arvore busca binaria

---
+++ arvores/arvores_binarias/src/ArvoreBuscaBinaria.java
@@ -0,0 +1,9 @@
+public class ArvoreBuscaBinaria {
+    public int dado;
+    public ArvoreBuscaBinaria esq;
+    public ArvoreBuscaBinaria dir;
+
+    public static ArvoreBuscaBinaria init() {
+        return null;
+    }
+}
