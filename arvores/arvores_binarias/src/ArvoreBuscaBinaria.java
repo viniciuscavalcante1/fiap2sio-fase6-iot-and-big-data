96ed88cfcd9e7f568c4bc901e0988ab8c0180864
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-10-14 16:58:44 -0300

    add remover

--- arvores/arvores_binarias/src/ArvoreBuscaBinaria.java
+++ arvores/arvores_binarias/src/ArvoreBuscaBinaria.java
@@ -21,4 +21,39 @@ public class ArvoreBuscaBinaria {
         }
         return a;
     }
+
+    public static ArvoreBuscaBinaria remove_valor(ArvoreBuscaBinaria a, int info) {
+        if (a != null) {
+            if (info == a.dado) {
+                if (a.esq == null && a.dir == null) {
+                    // nó folha
+                    return null;
+                }
+                if (a.esq == null) {
+                    // nó com um filho à direita
+                    return a.dir;
+                }
+                if (a.dir == null) {
+                    // nó com um filho à esquerda
+                    return a.esq;
+                }
+// nó com dois filhos
+                ArvoreBuscaBinaria aux, ref;
+                ref = a.dir;
+                aux = a.dir;
+                while (aux.esq != null) {
+                    aux = aux.esq;
+                }
+                aux.esq = a.esq;
+                return ref;
+            } else {
+                if (info < a.dado) {
+                    a.esq = remove_valor(a.esq, info);
+                }
+                else {
+                    a.dir = remove_valor(a.dir, info);
+                }
+            }
+        } return a;
+    }
 }
