b7a9ea94bd47f5fcfeaf8cb52a16b6862971f48a
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-10-14 15:33:28 -0300

    add inserir

--- arvores/arvores_binarias/src/ArvoreBuscaBinaria.java
+++ arvores/arvores_binarias/src/ArvoreBuscaBinaria.java
@@ -6,4 +6,19 @@ public class ArvoreBuscaBinaria {
     public static ArvoreBuscaBinaria init() {
         return null;
     }
+
+    public static ArvoreBuscaBinaria inserir(ArvoreBuscaBinaria a, int info) {
+        // insere um novo nó na árvore de busca binária
+        if (a == null) {
+            a = new ArvoreBuscaBinaria();
+            a.dado = info;
+            a.esq = null;
+            a.dir = null;
+        } else if (info < a.dado) {
+            a.esq = inserir(a.esq, info);
+        } else {
+            a.dir = inserir(a.dir, info);
+        }
+        return a;
+    }
 }
